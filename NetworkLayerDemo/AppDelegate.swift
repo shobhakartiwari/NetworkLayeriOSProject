6d0d3cf0b7d0e3efad4b48f2cdcb2916ef5fd5d1
testUser <shubh_tiwari@hotmail.com>
2020-12-30 23:15:19 +0530

    comment in appdelegate added

--- NetworkLayerDemo/AppDelegate.swift
+++ NetworkLayerDemo/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
+    //this is the first method which gets called in iOS application
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
